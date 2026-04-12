eb0e7c6b4b351ea7142805874e9991654168ace3
Julian Franco <juliandanielfranco@gmail.com>
2026-04-12 12:46:03 -0500

    feat: add cronjob for timing cobros y and recordatorios

--- test_prompt.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from services.groq_service import generate_felicitacion_with_groq, generate_cobro_with_groq
-
-async def main():
-    with open('test_output.txt', 'w', encoding='utf-8') as f:
-        f.write(await generate_felicitacion_with_groq() + '\n---\n')
-        f.write(await generate_cobro_with_groq('leve') + '\n')
-
-asyncio.run(main())
